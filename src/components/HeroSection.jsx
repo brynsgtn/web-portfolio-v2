27d534df400813eadc09275cfd3a2fb7589e562e
brynsgtn <bryansuguitan17@gmail.com>
2025-11-19 12:47:21 -0600

    Improve UI

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,12 +10,12 @@ const HeroSection = () => {
 
   return (
     <>
-      <div className="min-h-screen pt-50 relative">
+      <div className="min-h-screen pt-30 md:pt-40 relative">
         <p className="text-success tracking-wider font-light text-xl mb-5">Hi, my name is </p>
-        <h1 className="text-6xl md:text-7xl font-semibold text-primary-content mb-5">Bryan Suguitan.</h1>
+        <h1 className="text-5xl md:text-7xl font-semibold text-primary-content mb-5">Bryan Suguitan.</h1>
         <RotatingText
           texts={['Software Engineer', 'Full-Stack Developer', 'Frontend Developer', 'Backend Developer', 'QA Engineer']}
-          mainClassName="text-secondary-content font-semibold text-5xl md:text-6xl overflow-hidden py-0.5 sm:py-1 md:py-2 r rounded-lg w-fit"
+          mainClassName="text-secondary-content font-semibold text-3xl md:text-6xl overflow-hidden py-0.5 sm:py-1 md:py-2 r rounded-lg w-fit"
           staggerFrom={"last"}
           initial={{ y: "100%" }}
           animate={{ y: 0 }}
@@ -25,7 +25,7 @@ const HeroSection = () => {
           transition={{ type: "spring", damping: 30, stiffness: 400 }}
           rotationInterval={3000}
         />
-        <p className="max-w-2xl text-xl mt-5 font-extralight text-secondary-content">
+        <p className="max-w-2xl text-md md:text-xl mt-5 font-extralight text-secondary-content">
           I build full stack applications with attention to usability, stability, and security. I like integrating cloud services, writing tests, and improving code through modern engineering practices
         </p>
         <div className="mt-5">
@@ -35,7 +35,7 @@ const HeroSection = () => {
           <SocialIcon network="instagram" href="https://www.instagram.com/brynsgtn/" target="_blank" borderRadius="35%" className="ms-3" />
           <button
             onClick={() => window.open('/Suguitan-Bryan-CV(FSD).pdf', '_blank')}
-            className="btn border-success text-success ml-5 hover:text-base-100 hover:bg-success transition-colors duration-300 bg-transparent rounded-md"
+            className="btn border-success text-success md:ml-5 hover:text-base-100 hover:bg-success transition-colors duration-300 bg-transparent rounded-md block md:inline mt-5 md:mt-0"
           >
             Resume
           </button>
@@ -43,10 +43,10 @@ const HeroSection = () => {
 
         <button
           onClick={scrollToNext}
-          className="absolute bottom-10 left-1/2 -translate-x-1/2 px-6 py-3 border border-success/30 bg-success/10 backdrop-blur-sm rounded-md flex items-center gap-2 hover:bg-success/20 transition-all cursor-pointer"
+          className="absolute bottom-10 left-1/2 -translate-x-1/2 md:px-6 py-2 md:border md:border-success/30 md:bg-success/10 backdrop-blur-sm rounded-md flex items-center gap-2 hover:bg-success/20 transition-all cursor-pointer"
           aria-label="Scroll to next section"
         >
-          <span className="text-success text-sm font-medium">Explore More</span>
+          <span className="text-success text-sm font-medium hidden md:block">Explore More</span>
           <ChevronDown className="w-4 h-4 text-success animate-bounce" />
         </button>
       </div>
